dfe76d1a3cd81cf0544a34e3c7140021f3b57321
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-03-07 11:04:30 -0500

    Initial Commit

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,48 @@
+
+const AuthService = require('../auth/auth-service');
+
+function requireAuth(req, res, next) {
+
+  const authToken = req.get('Authorization') || ''
+
+  let bearerToken
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
+    return res.status(401).json({ error: 'Missing bearer token' })
+  } else {
+    bearerToken = authToken.slice(7, authToken.length)
+  }
+
+  const [tokenUserName, tokenPassword] = Buffer
+    .from(bearerToken, 'base64')
+    .toString()
+    .split(':');
+
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+
+  req.app.get('db')('thingful_users')
+    .where({ user_name: tokenUserName })
+    .first()
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+      }
+
+
+      return AuthService.comparePasswords(tokenPassword, user.password)
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: 'Unauthorized request' });
+          }
+
+          req.user = user;
+          next();
+        })
+        .catch(next);
+    });
+}
+
+module.exports = {
+  requireAuth,
+};
